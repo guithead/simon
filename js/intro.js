f893d69f26a9188249aa9f16e79a997ef4b27b83
guithead <62117784+guithead@users.noreply.github.com>
2020-11-16 18:17:46 +0100

    Add files via upload

--- js/intro.js
+++ js/intro.js
@@ -1,27 +1,18 @@
 $(document).ready(function () {
-  /*var sound = new Audio("sounds/theme.mp3");
-  sound.play();
-  sound.loop = true;
-
-  if (!sound.paused) {
-    $("#play-pause").attr("class", "fas fa-volume-mute");
-  }*/
-
   var sound = new Howl({
     src: ["sounds/theme.mp3"],
     loop: true,
   });
 
-  function boom() {
+  function playTheme() {
     if (sound.playing()) {
       $("#play-pause").attr("class", "fas fa-volume-up");
       sound.pause();
     } else {
       $("#play-pause").attr("class", "fas fa-volume-mute");
       sound.play();
-      //sound.loop = true;
     }
   }
 
-  $("#play-pause").on("click", boom);
+  $("#play-pause").on("click", playTheme);
 });
